39124c7b3ec2619ebc865f39bf6046cea135858d
melanyArgandona <melmune@gmail.com>
2024-08-30 09:06:18 -0400

    Add navigation to artist list and favorite songs

--- reporductorDeMusica/app/src/main/java/com/example/reporductordemusica/ui/listArtist/ArtistListActivity.kt
+++ reporductorDeMusica/app/src/main/java/com/example/reporductordemusica/ui/listArtist/ArtistListActivity.kt
@@ -1,10 +1,12 @@
 package com.example.reporductordemusica.ui.listArtist
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ListView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.reporductordemusica.FavoriteSongsActivity
 import com.example.reporductordemusica.R
 import com.example.reporductordemusica.ViewModel.ArtistListViewModel
 import com.example.reporductordemusica.ui.artist.ArtistAdapter
@@ -28,5 +30,10 @@ class ArtistListActivity : AppCompatActivity() {
         }
 
         artistListViewModel.fetchArtists()
+
+        findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.floatingActionButton4).setOnClickListener {
+            val intent = Intent(this, FavoriteSongsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
